66f163f53c8953ecccc74b03e47babf66957a92d
Victor <ciobavictor@gmail.com>
2025-04-15 23:08:42 +0300

    python back?

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+
+def get_book_text(path_to_file):
+        with open(path_to_file, 'r') as f:
+            text = f.read()
+        return text
+
+def main():
+    # Relative path to the Frankenstein text file
+    path = 'books/frankenstein.txt'
+    
+    # Get the full text of the book
+    book_text = get_book_text(path)
+    
+    # Print the entire contents to the console
+    print(book_text)
+    return main
