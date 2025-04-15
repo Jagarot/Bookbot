7dd8d3e52812f449adcbff122f56597add0737e9
Victor <ciobavictor@gmail.com>
2025-04-16 00:16:25 +0300

    final?

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ def get_book_text(path_to_file):
         text = f.read()  # Read the entire content of the file
     return text
 
-def number_of_words(content):  # Accept the content as an argument
+def number_of_words(content):  
     words = content.split()  # Split the content into words based on whitespace
-    word_count = len(words)  # Count the number of words
-    return word_count  # Return the word count
+    num_words = len(words)  # Count the number of words 
+    return num_words  # Return the word count
+   
 
 def main():
     # Relative path to the Frankenstein text file
     path = 'books/frankenstein.txt'
     
-    # Get the full text of the book
+    # the text
     book_text = get_book_text(path)
     
     # Count the number of words in the book
-    word_count = number_of_words(book_text)
+    num_words = number_of_words(book_text)
     
     # Print the word count to the console
-    print(f"Word count: {word_count}")
+    print (f"{num_words} words found in the document")
 
 # Don't forget this line!
 main()
